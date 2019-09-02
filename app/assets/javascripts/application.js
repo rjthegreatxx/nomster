7c292100754b1a365d4c77f783fb0b0cd2a36f12
RJ Militante <redmond007@gmail.com>
2019-09-02 12:28:05 -0500

    fix jquqery, comments modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require turbolinks
 //= require popper
 //= require tether
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require_tree .
